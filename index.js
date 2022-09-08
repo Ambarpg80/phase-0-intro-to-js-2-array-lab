fa9c4b2381ccb1900aca36f9d1cc3b5e9cf8d376
Ambar <Ambarpg80@gmail.com>
2022-09-08 10:28:01 -0400

    commit tree 19d86eb794ee693ece362d18e115eeb4075a7cb2

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+   cats.push(name);
+}
+
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+ }
+ 
+function destructivelyRemoveLastCat(){
+    cats.pop();
+ }
+
+ function destructivelyRemoveFirstCat(){
+    cats.shift();
+ }
+
+function appendCat(name){
+    const copyCats = [...cats, name]
+    return copyCats;
+}
+function prependCat(name){
+    const moreCopyCats = [name,...cats]
+    return moreCopyCats;
+}
+
+function removeLastCat(){
+    const takingCats = cats.slice(0,2);
+    return takingCats;
+}
+function removeFirstCat(){
+    const takingMoreCats = cats.slice(1);
+    return takingMoreCats;
+}
+//function 
+//function 
+//function 
+//function
